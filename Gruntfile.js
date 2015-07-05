50cd6dd3fd380e8544992f236bdb8a0d546391e5
Rafael Nowrotek <mail@benignware.com>
2015-07-05 22:58:52 +0200

    Added grunt-livemd

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,18 +5,20 @@ module.exports = function(grunt) {
 
     copy: {
       dist: {
-        expand: true, cwd: 'src/', src: ['js/**'], dest: 'dist/'
+        expand: true, cwd: 'src', src: ['**/*'], dest: 'dist'
+      },
+      site: {
+        expand: true, cwd: 'dist/', src: ['**/*'], dest: 'site/'
       }
     },
 
     // Lint definitions
     jshint: {
-      files: ["src/**"],
+      all: ["src/**.js"],
       options: {
         jshintrc: ".jshintrc"
       }
     },
-
     uglify: {
       options: {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
@@ -27,15 +29,25 @@ module.exports = function(grunt) {
         }
       }
     },
-    jekyll_docs: {
-      build: {
-        dest: 'docs',
-        cwd: 'src',
-        expand: true,
-        src: [
-          '../README.md',
-          '**/*.{md,js,css}'
-        ]
+    livemd: {
+      options: {
+      },
+      site: {
+        files: {
+          'site/index.html': ['README.md']
+        }
+      }
+    },
+    'gh-pages': {
+      options: {
+        // Options for all targets go here.
+      },
+      site: {
+        options: {
+          base: 'site'
+        },
+        // These files will get pushed to the `gh-pages` branch (the default).
+        src: ['**/*']
       }
     }
   });
@@ -43,9 +55,13 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-jekyll-docs');
+  grunt.loadNpmTasks("grunt-livemd");
+  grunt.loadNpmTasks("grunt-gh-pages");
 
-  grunt.registerTask('default', ['jshint', 'copy', 'uglify']);
-  grunt.registerTask('docs', ['jekyll_docs']);
+  grunt.registerTask('default', ['copy:dist', 'jshint', 'uglify']);
+  
+  grunt.registerTask('build', ['copy:dist', 'uglify']);
+  
+  grunt.registerTask('site', ['build', 'copy:site', 'livemd:site']);
 
 };
